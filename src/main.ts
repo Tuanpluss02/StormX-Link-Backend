1135175be4641fb235ab499f3a28020abeadf2ef
Storm X <62681489+StormX02@users.noreply.github.com>
2023-11-17 08:51:13 +0000

    (chore) remove trash

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import chalk from "chalk";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
