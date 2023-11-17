222f80406f092d2d8447979df2f05962a254da05
Storm X <62681489+StormX02@users.noreply.github.com>
2023-11-17 08:44:55 +0000

    (chore) make color🤡

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import chalk from "chalk";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1/docs', app, document);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('\x1b[33m%s\x1b[0m', '🔥 Server is running on port 3000');
+    console.log('\x1b[36m%s\x1b[0m', '🚀 See docs at http://localhost:3000/api/v1/docs');
+  });
 }
 bootstrap();
