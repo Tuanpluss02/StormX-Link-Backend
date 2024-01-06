fbfe63984cdcce5f6794160d82ac6794c30dcd27
Storm X <62681489+StormX02@users.noreply.github.com>
2024-01-06 04:58:47 +0000

    (chore) update urlcode format

---
+++ src/utilities/urlCodeGenerator.ts
@@ -0,0 +1,9 @@
+export const genUrlCode = () : string => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+}
