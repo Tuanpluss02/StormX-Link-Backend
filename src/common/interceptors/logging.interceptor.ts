f94c761afc25358c91d779ea8907b4f9b2418ba2
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2025-01-13 11:17:55 +0700

    (refactor): update mongo version

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -13,7 +13,7 @@ export class LoggingInterceptor implements NestInterceptor {
 
   intercept(
     context: ExecutionContext,
-    next: CallHandler<any>,
+    next: CallHandler,
   ): Observable<any> | Promise<Observable<any>> {
     const request = context.switchToHttp().getRequest();
     const userAgent = request.get("user-agent") || "";
@@ -21,7 +21,7 @@ export class LoggingInterceptor implements NestInterceptor {
     const now = Date.now();
 
     return next.handle().pipe(
-      tap((res) => {
+      tap(() => {
         const response = context.switchToHttp().getResponse();
         const { statusCode } = response;
         this.logger.log(
